492322781cab647f2b99bb27908f5adc1c75c183
Bruno <brunorodrigues.silveira@gmail.com>
2020-09-19 08:19:28 -0300

    Adiciona lombok

--- app/src/main/java/com/example/meutalento/models/UserAccountSettings.java
+++ app/src/main/java/com/example/meutalento/models/UserAccountSettings.java
@@ -1,5 +1,10 @@
 package com.example.meutalento.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
 public class UserAccountSettings {
 
     private String description;
@@ -22,86 +27,5 @@ public class UserAccountSettings {
         this.username = username;
         this.website = website;
     }
-    public UserAccountSettings() {
 
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDscription(String description) {
-        this.description = description;
-    }
-
-    public String getDisplay_name() {
-        return display_name;
-    }
-
-    public void setDisplay_name(String display_name) {
-        this.display_name = display_name;
-    }
-
-    public long getFollowers() {
-        return followers;
-    }
-
-    public void setFollowers(long followers) {
-        this.followers = followers;
-    }
-
-    public long getFollowing() {
-        return following;
-    }
-
-    public void setFollowing(long following) {
-        this.following = following;
-    }
-
-    public long getPosts() {
-        return posts;
-    }
-
-    public void setPosts(long posts) {
-        this.posts = posts;
-    }
-
-    public String getProfile_photo() {
-        return profile_photo;
-    }
-
-    public void setProfile_photo(String profile_photo) {
-        this.profile_photo = profile_photo;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getWebsite() {
-        return website;
-    }
-
-    public void setWebsite(String website) {
-        this.website = website;
-    }
-
-
-    @Override
-    public String toString() {
-        return "UserAccountSettings{" +
-                "description='" + description + '\'' +
-                ", display_name='" + display_name + '\'' +
-                ", followers=" + followers +
-                ", following=" + following +
-                ", posts=" + posts +
-                ", profile_photo='" + profile_photo + '\'' +
-                ", username='" + username + '\'' +
-                ", website='" + website + '\'' +
-                '}';
-    }
 }
